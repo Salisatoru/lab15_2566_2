ffe68ac683aa25c4044a92aaab8e187b5289f06e
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2024-01-26 15:26:06 +0700

    Completed lab15_4.cpp

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,20 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+    int N[4] = {};
+    int i = rand()%4;
+    N[i]=50;
+    do{i = rand()%4;}while(N[i]!=0);
+    N[i]=100;
+    do{i = rand()%4;}while(N[i]!=0);
+    N[i]=500;
+    do{i = rand()%4;}while(N[i]!=0);
+    N[i]=1000;
+    a=N[0];
+    b=N[1];
+    c=N[2];
+    d=N[3];
+    
+}
